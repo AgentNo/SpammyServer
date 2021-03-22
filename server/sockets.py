f80c55fef5569dc00c9f8ee52ef264a2a1c6c921
AgentNo <dfaulkner2010@hotmail.co.uk>
2021-03-22 22:23:20 +0000

    Chapter 6 base implementation

--- server/sockets.py
+++ server/sockets.py
@@ -9,12 +9,34 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.listen(5) # Sets max backlog number to 5
     conn, addr = sock.accept()
     
-    conn.send(b"Hello Client! You are now connected to the echo server. Type something and the server will echo it back.\n")
+    conn.send(b"Hello Client! You are now connected to the dictionary server. Add or search for a definition.\n")
+    defs = dict()
+    defs["def"] = "Hello, this is a test definition! If you are seeing this, then the protocol works!"
     while True:
         reply = conn.recv(1024)
         if not reply:
             break
-        conn.send(b"\nYou said: \n") 
-        conn.sendall(reply) # Sendall blocks to ensure all data has been sent
+
+        # Parse the response and obtain arguments
+        reply_str = reply.decode("UTF-8")
+        reply_split = reply_str.split()
+        command, arg = None, None
+
+        if len(reply_split) >= 2:
+            command = reply_split[0]
+            arg = reply_split[1]
+            if command.upper() == "GET":
+                if arg in defs.keys():
+                    definition = str(defs[arg])
+                    # Respond to the client again
+                    conn.send(b"\nDefinition:\n") 
+                    conn.sendall(bytes(definition, "UTF-8"))
+                    conn.send(b"\n\n")
+                else:
+                    conn.send(b"\nERROR - Definition not found.\n")
+            else:
+                conn.send(b"\nERROR - Command not supported.\n")
+        else: 
+            conn.send(b"\nERROR - Not enough arguments given - requred in format 'VERB <argument>'\n")
 
     conn.close()
