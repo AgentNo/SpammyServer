594df2f1a51ae8539ca6cf10c8671252d99883fb
AgentNo <dfaulkner2010@hotmail.co.uk>
2021-03-28 13:24:02 +0100

    Chapter 6 Bonus implementation

--- server/sockets.py
+++ server/sockets.py
@@ -2,6 +2,58 @@
 
 import socket
 
+# GET a definition from the dictionary
+def get_defition(reply_split):
+    arg = reply_split[1] # Will only return the first definition if multiple are passed
+    if arg in defs.keys():
+        definition = str(defs[arg])
+        # Respond to the client again
+        conn.send(b"\nDefinition:\n") 
+        conn.sendall(bytes(definition, "UTF-8"))
+        conn.send(b"\n\n")
+    else:
+        # Defitinion not found
+        conn.send(b"\nERROR undefined.\n")
+
+
+# SET a new defition in the dictionary
+def set_defition(reply_split):
+    # Format is <command> <word> <defs (multiple words)>
+    word = reply_split[1]
+    defs[word] = " ".join(reply_split[2:])
+    conn.send(b"\nDefinition created in dictionary.\n\n")
+
+
+# DELETE a single definition in the dictionary
+def delete_definition(reply_split):
+    word = reply_split[1]
+    if word in defs.keys():
+        del defs[word]
+        reply = bytes("{} deleted from dictionary\n\n".format(word), "UTF-8")
+        conn.send(reply)
+    else:
+        conn.send(b"ERROR word undefined.")
+
+
+# CLEAR all definitions in the dictionary
+def clear_definitions():
+    length = len(defs)
+    defs.clear()
+    reply = "\n{} definitions cleared.\n\n".format(length)
+    conn.send(bytes(reply, "UTF-8")) 
+
+
+# GET ALL definitions from the dictionary
+def all_definitions():
+    # String all definitions together and send as one string
+    reply = ""
+    for key in defs:
+        definition = "\n{}: {}\n".format(key, defs[key])
+        reply.join(definition)
+    reply.join("\n\n")
+    conn.sendall(bytes(reply, "UTF-8"))
+
+
 # AF_INET = IPv4 address family, SOCK_STREAM = Socket type for TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind(('127.0.0.1', 14533))
@@ -10,7 +62,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     
     conn.send(b"Hello Client! You are now connected to the dictionary server. Add or search for a definition.\n")
     defs = dict()
-    defs["def"] = "Hello, this is a test definition! If you are seeing this, then the protocol works!"
     while True:
         reply = conn.recv(1024)
         if not reply:
@@ -19,23 +70,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         # Parse the response and obtain arguments
         reply_str = reply.decode("UTF-8")
         reply_split = reply_str.split()
-        command, arg = None, None
+        command = reply_split[0].upper()
 
-        if len(reply_split) >= 2:
-            command = reply_split[0]
-            arg = reply_split[1]
-            if command.upper() == "GET":
-                if arg in defs.keys():
-                    definition = str(defs[arg])
-                    # Respond to the client again
-                    conn.send(b"\nDefinition:\n") 
-                    conn.sendall(bytes(definition, "UTF-8"))
-                    conn.send(b"\n\n")
-                else:
-                    conn.send(b"\nERROR - Definition not found.\n")
-            else:
-                conn.send(b"\nERROR - Command not supported.\n")
-        else: 
-            conn.send(b"\nERROR - Not enough arguments given - requred in format 'VERB <argument>'\n")
+        if command == "GET" and len(reply_split) >= 2:
+            get_defition(reply_split)
+        elif command.upper() == "SET" and len(reply_split) >= 3:
+            set_defition(reply_split)
+        elif command.upper() == "DELETE" and len(reply_split) >= 2:
+            delete_definition(reply_split)
+        elif command.upper() == "CLEAR":
+            clear_definitions()
+        elif command.upper() == "ALL":
+            all_definitions()
+        else:
+            conn.send(b"\nERROR command undefined.\n")
+
 
     conn.close()
